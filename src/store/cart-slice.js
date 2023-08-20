95a056547b3866e8c8ec5395e61a914dd9f7fd40
Arindam <bis.ips22@gmail.com>
2023-08-20 14:06:44 +0530

    Cart items added to firebase with notifications

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -7,6 +7,10 @@ const cartSlice = createSlice({
     totalQuantity: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -14,13 +18,13 @@ const cartSlice = createSlice({
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
-          name: newItem.title,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
+          name: newItem.title,
         });
       } else {
-        existingItem.quantity = existingItem.quantity + 1;
+        existingItem.quantity++;
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
@@ -32,11 +36,11 @@ const cartSlice = createSlice({
         state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
-        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
 });
+
 export const cartActions = cartSlice.actions;
 
 export default cartSlice;
