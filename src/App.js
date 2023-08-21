dceedcf44584ad5e1525b4bf1a2e62260b9005d9
Arindam <bis.ips22@gmail.com>
2023-08-21 12:40:52 +0530

    Modified using an action creator thunk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { Fragment, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
-import Notification from "./components/UI/Notification";
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-let isinitial = true;
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
+
+let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
@@ -15,49 +16,12 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending Cart Data",
-        })
-      );
-      const response = await fetch(
-        "https://redux-cart-4dd3a-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent Cart Data Successfully",
-        })
-      );
-    };
-
-    if (isinitial) {
-      isinitial = false;
+    if (isInitial) {
+      isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Error sending Cart Data ",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
